d8457109933adf3f7f9c9117173431def1621b0c
vorgestern <vorgestern@niemals>
2024-11-16 23:04:29 +0100

    LuaFils: Erweiterung (pwd)

--- demo.lua
+++ demo.lua
@@ -3,3 +3,5 @@ local X=require "luafils"
 
 print "luafils:"
 for k,v in pairs(X) do print("==", k, v) end
+
+print("pwd="..X.pwd())
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,26 @@
 #include <lua.hpp>
 #include <LuaAide.h>
 
+#include <filesystem>
+
+using namespace std;
+
+namespace {
+extern "C" int pwd(lua_State*L)
+{
+    LuaStack Q(L);
+    auto X=filesystem::current_path().string();
+    Q<<X.c_str();
+    return 1;
+}
+} // anon
+
 extern "C" int luaopen_luafils(lua_State*L)
 {
     LuaStack Q(L);
     Q   <<LuaTable()
         <<"0.1">>LuaField("version")
-        <<"https://github.com/vorgestern/LuaFils.git">>LuaField("url");
+        <<"https://github.com/vorgestern/LuaFils.git">>LuaField("url")
+        <<pwd>>LuaField("pwd");
     return 1;
 }
